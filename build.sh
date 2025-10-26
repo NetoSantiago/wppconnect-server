2e3375cb7f0156dca010abac836fb71ba41effbd
Santiago <santiagoneto11@gmail.com>
2025-10-25 22:43:02 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 yarn install
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get update
 apt-get install libappindicator1
-dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -y chromium
+
